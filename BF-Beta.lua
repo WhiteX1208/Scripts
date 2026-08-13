f6d3256f070daa645729e7769fd13376a335a2ea
Minh Nghia <nguyenhoanggianamk8@gmail.com>
2026-08-13 02:32:17 -0700

    Update BF-Beta.lua

--- BF-Beta.lua
+++ BF-Beta.lua
@@ -1,9 +1,2 @@
 repeat wait() until game:IsLoaded()
-local ScriptUrl = "https://raw.githubusercontent.com/WhiteX1208/Scripts/refs/heads/main/BloxFruits.luau"
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Night Hub",
-    Text = "New Link Script Copied to your clipboard!"
-    Duration = 5,
-})
-setclipboard([[loadstring(game:HttpGet("https://raw.githubusercontent.com/WhiteX1208/Scripts/refs/heads/main/BloxFruits.luau"))()]])
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/WhiteX1208/Scripts/refs/heads/main/BloxFruits.luau"))()
